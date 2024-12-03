c3bbe0fa03154f6aaba36da9fc459c5709749f0d
Robin Weller <robin@saucecode.co>
2024-12-03 11:47:40 +0000

    Day 03

---
+++ Day03/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllText("input.txt");
+
+const string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+
+var matches = Regex.Matches(lines, pattern);
+
+var list = new List<string>();
+
+foreach (var match in matches)
+{
+    list.Add(match.ToString()!);
+}
+
+var taskOneSum = list.Sum(Multiply);
+var taskTwoSum = MultiplyIfEnabled(lines);
+
+Console.WriteLine(taskOneSum);
+Console.WriteLine(taskTwoSum);
+
+return;
+
+int Multiply(string input)
+{
+    input = input.Replace("mul(", "").Replace(")", "");
+    var values = input.Split(",");
+    return int.Parse(values[0]) * int.Parse(values[1]);
+}
+
+int MultiplyIfEnabled(string input)
+{
+    var total = 0;
+    var enabled = true;
+    var charArray = input.ToCharArray();
+    for (var i = 0; i < charArray.Length-12; i++)
+    {
+        switch (charArray[i])
+        {
+            case 'd' when input.Substring(i, 7).Equals("don't()"):
+                enabled = false;
+                break;
+            case 'd':
+            {
+                if (input.Substring(i, 4).Equals("do()"))
+                    enabled = true;
+                break;
+            }
+            case 'm' when enabled:
+            {
+                const string regexPattern = @"mul\(\d{1,3},\d{1,3}\)";
+                var substring = input.Substring(i, 12);
+                var matchCollection = Regex.Matches(substring, regexPattern);
+                if (matchCollection.Count == 0) continue;
+                total += Multiply(matchCollection.First().ToString());
+                break;
+            }
+        }
+    }
+    return total;
+}
